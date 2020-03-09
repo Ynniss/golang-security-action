ec90a42ae4fe327d566a7e471058b347b8b03a0a
YannisMekaouche <mekaou_y@etna-alternance.net>
2020-03-09 22:59:26 +0100

    added './' before script name + added condition to launch gosec regarding whether env variables are defined or not

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-gosec $CODE_PATH
+if [ -z "$CODE_PATH" ] && [ -z "$CONF_PATH" ] then
+    gosec -conf $CONF_PATH $CODE_PATH
+elif  [ -z "$CODE_PATH" ]
+    gosec $CODE_PATH
+elif  [ -z "$CONF_PATH" ]
+    gosec -conf $CONF_PATH $CODE_PATH
+fi
