8f31d93cb954c1178f4c4ec075871a8aa0b606f6
YannisMekaouche <mekaou_y@etna-alternance.net>
2020-03-09 23:12:24 +0100

    test without permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-if [ -z "$CODE_PATH" ] && [ -z "$CONF_PATH" ]
-then
-    gosec -conf $CONF_PATH $CODE_PATH
-elif  [ -z "$CODE_PATH" ]
-then
-    gosec $CODE_PATH
-elif  [ -z "$CONF_PATH" ]
-then
-    gosec -conf $CONF_PATH $CODE_PATH
-fi
+# if [ -z "$CODE_PATH" ] && [ -z "$CONF_PATH" ]
+# then
+#     gosec -conf $CONF_PATH $CODE_PATH
+# elif  [ -z "$CODE_PATH" ]
+# then
+#     gosec $CODE_PATH
+# elif  [ -z "$CONF_PATH" ]
+# then
+#     gosec -conf $CONF_PATH
+# fi
+
+gosec
