9a68e23733234b82e9b28b5b4e060796589c8bee
YannisMekaouche <mekaou_y@etna-alternance.net>
2020-03-09 23:04:36 +0100

    fixed script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-if [ -z "$CODE_PATH" ] && [ -z "$CONF_PATH" ] then
+if [ -z "$CODE_PATH" ] && [ -z "$CONF_PATH" ]
+then
     gosec -conf $CONF_PATH $CODE_PATH
 elif  [ -z "$CODE_PATH" ]
+then
     gosec $CODE_PATH
 elif  [ -z "$CONF_PATH" ]
+then
     gosec -conf $CONF_PATH $CODE_PATH
 fi
