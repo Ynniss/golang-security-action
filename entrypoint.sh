beca7c3952c60b0734273bf84f05fa0632904235
YannisMekaouche <mekaou_y@etna-alternance.net>
2020-03-10 21:25:21 +0100

    fix env variable on script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 launchGosec() {
-    if [ -n "$CODE_PATH" ] && [ -n "$CONF_PATH" ]; then
-        gosec -conf $CONF_PATH $CODE_PATH
-    elif [ -n "$CODE_PATH" ]; then
-        gosec $CODE_PATH
-    elif [ -n "$CONF_PATH" ]; then
-        gosec -conf $CONF_PATH
+    if [ -n "$INPUT_CODE_PATH" ] && [ -n "$INPUT_CONF_PATH" ]; then
+        gosec -conf $INPUT_CONF_PATH $INPUT_CODE_PATH
+    elif [ -n "$INPUT_CODE_PATH" ]; then
+        gosec $INPUT_CODE_PATH
+    elif [ -n "$INPUT_CONF_PATH" ]; then
+        gosec -conf $INPUT_CONF_PATH
     fi
 }
 
