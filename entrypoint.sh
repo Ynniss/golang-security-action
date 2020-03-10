733e79464ab323e50b276725088707381f340e16
YannisMekaouche <mekaou_y@etna-alternance.net>
2020-03-10 20:45:02 +0100

    testing condition to see if variables exists or not

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# if [ -z "$CODE_PATH" ] && [ -z "$CONF_PATH" ]
-# then
-#     gosec -conf $CONF_PATH $CODE_PATH
-# elif  [ -z "$CODE_PATH" ]
-# then
-#     gosec $CODE_PATH
-# elif  [ -z "$CONF_PATH" ]
-# then
-#     gosec -conf $CONF_PATH
-# fi
+if [ -z "$CODE_PATH" ] && [ -z "$CONF_PATH" ]
+then
+    echo "CODE_PATH ET CONF_PATH" 
+    # gosec -conf $CONF_PATH $CODE_PATH
+elif  [ -z "$CODE_PATH" ]
+then
+    # gosec $CODE_PATH
+    echo "CODE_PATH" 
+elif  [ -z "$CONF_PATH" ]
+then
+    echo "CONF_PATH" 
+    # gosec -conf $CONF_PATH
+fi
 
-gosec test.go
+# gosec test.go
