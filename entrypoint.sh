404fb8845c8b942151505335318128f737c6a2e5
YannisM <mekaou_y@etna-alternance.net>
2020-03-11 20:59:48 +0100

    Update entrypoint.sh
    cleaned condition, regarding CODE_PATH is required:true

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 launchGosec() {
-    if [ -n "$INPUT_CODE_PATH" ] && [ -n "$INPUT_CONF_PATH" ]; then
+    if [ -n "$INPUT_CONF_PATH" ]; then
         gosec -conf $INPUT_CONF_PATH $INPUT_CODE_PATH
-    elif [ -n "$INPUT_CODE_PATH" ]; then
+    else
         gosec $INPUT_CODE_PATH
-    elif [ -n "$INPUT_CONF_PATH" ]; then
-        gosec -conf $INPUT_CONF_PATH
     fi
 }
 
